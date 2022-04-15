52bb717aa1920a8243887edb61d888178fe5ddc6
duynam08 <duynam11a11999@gmail.com>
2022-04-15 09:27:00 +0700

    update count

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Components from './components/Component';
 import logo from './logo.svg';
 import User from './components/user';
 import Component from './components/Component';
+import { useState } from 'react';
 
 function App() {
+  const [defaultNumber,setdefaultNumber] =useState(1000)
   return (
     <div className="App">
-      
-         <Component />
+      <Component defaultNumber={defaultNumber} />
     </div>
   );
 }
--- src/components/Component.js
+++ src/components/Component.js
@@ -2,77 +2,24 @@ import { type } from "@testing-library/user-event/dist/type";
 import React, { useState } from "react";
 
 
-function Component(props) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [showpassword, setshowpassword]=useState(false)
-    
-    const handleChangeUsername = (e) => {
-        const value=e.target.value
-        setUsername(value);
-    };
-    const handleChangePassword = (e) => {
-      const value = e.target.value;
-      setPassword(value);
-    };
+function Component({defaultNumber}) {
 
-   const handleOnclick = (e) => {
-       console.log({ username, password });
-   };
-
-   const togglePassword = (e) => {
-       setshowpassword(!showpassword);
-       console.log(showpassword);
-   };
+    const [number, setnumber] = useState(defaultNumber);
+    const increase = (e) => {
+    //    const newNumber =number +1
+        setnumber(number + 1);
+    };
+   
+    const decrease = (e) => {
+      const newNumber = number - 1;
+      setnumber(newNumber);
+    };
 
   return (
     <div className="container">
-      <div>
-        <input
-          style={{
-            margin: "15px 12px",
-            padding: "4px",
-          }}
-          type="text"
-          placeholder="username"
-          value={username}
-          onChange={handleChangeUsername}
-        />
-      </div>
-      <div>
-        <input
-          style={{
-            margin: "15px 12px",
-            padding: "4px",
-          }}
-          type={showpassword === false ? "password" : "text"}
-          placeholder="password"
-          value={password}
-          onChange={handleChangePassword}
-        />
-        <br />
-        <button
-          style={{
-            margin: "1px 12px",
-            padding: "4px",
-          }}
-          type="checkbox"
-          onClick={togglePassword}
-        >
-          show
-        </button>
-      </div>
-      <div>
-        <button
-          style={{
-            margin: "1px 12px",
-            padding: "4px",
-          }}
-          onClick={handleOnclick}
-        >
-          Submit
-        </button>
-      </div>
+      <button onClick={increase}>increase +1 </button>
+      <input type="text" value={number}></input>
+      <button onClick={decrease}>decrease -1</button>
     </div>
   );
 }

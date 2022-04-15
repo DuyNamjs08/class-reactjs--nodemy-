cf6e1eff89bf0c9e2a913f9c5dc0ce7c73c6e488
duynam08 <duynam11a11999@gmail.com>
2022-04-15 15:48:48 +0700

    add emoji

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import Components from './components/Component';
-import logo from './logo.svg';
-import User from './components/user';
 import Component from './components/Component';
-import { useState } from 'react';
 
 function App() {
-  const [defaultNumber,setdefaultNumber] =useState(1000)
   return (
     <div className="App">
-      <Component defaultNumber={defaultNumber} />
+      <Component />
     </div>
   );
 }
--- src/components/Component.js
+++ src/components/Component.js
@@ -1,32 +1,12 @@
-import { type } from "@testing-library/user-event/dist/type";
 import React, { useState } from "react";
-
-function Component({ defaultNumber }) {
-  const [showHidebackground, setShowhide] = useState(false);
-  const toggle = (e) => {
-    setShowhide(!showHidebackground);
+function Component() {
+    const [emoji,setemoji] = useState(0)
+  const onclick = () => {
+     setemoji(emoji+1);
   };
   return (
     <div className="container">
-      <div
-        style={
-          showHidebackground
-            ? {
-                width: "100px",
-                height: "100px",
-                color: "black",
-                fontSize: "18px",
-                backgroundColor: " #FFFFFF",
-                backgroundImage:
-                  "linear-gradient(180deg, #FFFFFF 0%, #6284FF 50%, #FF0000 100%)",
-                  borderRadius:'50%'
-              }
-            : { display: "none" }
-        }
-      >
-        Hello các bạn{" "}
-      </div>
-      <button onClick={toggle}>show/hide</button>
+      <div onClick={onclick}>🥰 {emoji} </div>
     </div>
   );
 }

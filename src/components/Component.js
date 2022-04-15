697c7f5a20731414a8216923cec451bdb85918b9
duynam08 <duynam11a11999@gmail.com>
2022-04-15 09:44:22 +0700

    update wallet

--- src/components/Component.js
+++ src/components/Component.js
@@ -4,22 +4,39 @@ import React, { useState } from "react";
 
 function Component({defaultNumber}) {
 
-    const [number, setnumber] = useState(defaultNumber);
+    const [wallet, setwallet] = useState({
+        address:'Hà Nam',
+        name:'Nam',
+        property:1000000
+    });
     const increase = (e) => {
-    //    const newNumber =number +1
-        setnumber(number + 1);
+       
+        setwallet({
+          ...wallet,
+          property: wallet.property + 1000,
+        });
     };
    
     const decrease = (e) => {
-      const newNumber = number - 1;
-      setnumber(newNumber);
+      
+        setwallet({
+          ...wallet,
+          property: wallet.property - 10000,
+        });
+    };
+    const handleAlert = (mess) => {
+        alert(mess)
     };
 
   return (
     <div className="container">
-      <button onClick={increase}>increase +1 </button>
-      <input type="text" value={number}></input>
-      <button onClick={decrease}>decrease -1</button>
+      <button onClick={increase}>increase + 1000 </button>
+      <h2>Wallet</h2>
+      <div>{wallet.address}</div>
+      <div>{wallet.name}</div>
+      <div>{wallet.property}</div>
+      <button onClick={decrease}>decrease - 10000</button>
+      <button onClick={(e)=>{handleAlert('chào các bạn ')}}>click me</button>
     </div>
   );
 }
